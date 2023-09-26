255e2b7dd0f861067b6b24a9c7bea40461219a8c
lovesainju-7 <sainju.love69@gmail.com>
2023-09-26 22:18:25 +0545

    a bit changes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
     return (
+      <div>
       <p> Love sainju ko dashboard </p>
+
+
+      </div>
     )
   }
   
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage =() => { 
     return (
-        <div>
-            Love Shambhu Ssaainju 
 
-        </div>
+        <div>
+            <Link href="/sign-in">
+                <Button>
+                    login
+                </Button>
+            </Link>
+            <Link href="/sign-up">
+                <Button>
+                    Register
+                </Button>
+            </Link>
+        </div>     
+
     )
 };
 export default LandingPage;
