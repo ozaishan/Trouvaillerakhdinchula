5c8186a06bbb0ee7b876f4d3850dfa87d3692340
Bigyan <bigyan@Bigyans-MacBook-Pro.local>
2023-09-27 23:51:00 +0545

    Dashboard created, label made

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,33 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
-export default function page() {
-    return (
-      <div>
-      <p> Love sainju ko dashboard </p>
+import { MessageSquare } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+    href: "/conversation",
+  },
+]
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font bold text-center">
+          The Best AI Multimedia Generator
+        </h2>
+        <p className="text-muted-foreground font-;ight text-sm md:text-lg text-center">
+           Generate Contents With The Latest AI Technology
+        </p>
       </div>
-    )
-  }
+      <div className ="px-4 md:px-20 lg:px-32 space-y-4">
+
+      </div>
+    </div>
+  )
+}
   
+export default DashboardPage;
