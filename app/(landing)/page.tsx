78ad0b296e099bebd27a8b197f0d28357a4753db
Ishan Ojha <ojhaishan007@gmail.com>
2023-11-18 15:13:07 +0545

    video worked

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,18 @@
+import { LandingNavbar } from "@/components/landing-navbar";
+import { Landingkomainelement } from "@/components/ui/landing-main";
+
+
+const LandingPage =() => { 
+    return (
+<div className="h-full">
+  <div>
+<LandingNavbar />
+<Landingkomainelement />
+
+</div>
+</div>
+       
+
+    )
+};
+export default LandingPage;
